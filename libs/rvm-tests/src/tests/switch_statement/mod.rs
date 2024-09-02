47e14fe31105ba1c8a4b3141ab01b1617672d04e
notalpha <yan.gyunashyan@gmail.com>
2024-09-02 17:53:22 +0200

    switch test

--- libs/rvm-tests/src/tests/switch_statement/mod.rs
+++ libs/rvm-tests/src/tests/switch_statement/mod.rs
@@ -1,24 +1,23 @@
-//#[test]
-//fn basic_switch() {
-//	let runtime = launch(1024, vec![]);
-//
-//	compile(
-//		&runtime,
-//		&[
-//			("SwitchTest.java", include_str!("SwitchTest.java")),
-//			("Assert.java", include_str!("../Assert.java")),
-//		],
-//	)
-//	.unwrap();
-//	let (binding, identifier) = java_binding!(
-//		fn yes(value: bool) {
-//			assert!(value);
-//		}
-//	);
-//	println!("{identifier:?}");
-//	runtime.bindings.write().insert(identifier, binding);
-//
-//	let java = java_bind_method!(runtime fn tests::switch_statement::SwitchTest:test());
-//	let i = java();
-//}
-//
+use crate::launch;
+use rvm_runtime::bind;
+use std::sync::Arc;
+pub struct SwitchTest;
+bind!("tests/switch_statement" {
+	SwitchTest {
+		testSwitch(i: i32) -> i32
+	}
+});
+
+#[test]
+fn basic_switch() {
+	let runtime = launch(1024, vec!["tests/switch_statement/SwitchTest.class"]);
+	let test_switch = SwitchTest::testSwitch(&runtime);
+
+	assert_eq!(test_switch(0), 420);
+	assert_eq!(test_switch(1), 3);
+	assert_eq!(test_switch(2), 2);
+	assert_eq!(test_switch(3), 420);
+	assert_eq!(test_switch(4), 4);
+	assert_eq!(test_switch(5), 420);
+	assert_eq!(test_switch(10), 10);
+}
